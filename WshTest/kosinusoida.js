5ac381a0b7d8982fdc27a42df3d04b5ae79d6a9d
ibiliskov <ivan.biliskov@gmail.com>
2020-07-29 00:47:27 +0200

    Add test script

---
+++ WshTest/kosinusoida.js
@@ -0,0 +1,15 @@
+// Racuna se y vrijednost za broj na x osi
+// Poziva se methoda SetVal koja je definirana u host aplikaciji koja prima 2 argumenta
+for (i = 0; i < 500; i++)
+  app.SetVal(i, Math.cos(i * Math.PI / 90) * 200 + 220);
+
+// Renderiranje
+app.Invalidate();
+
+// Dohvacanje vrijednosti za vrijednost na osi x
+// Poziva se funkcija GetVal iz host-a koja prima vrijednost za os x i vraca vrijednost na osi y
+x = 20;
+y = app.GetVal(x);
+
+// Poziva se MessageBoxA koja je definirana u user32.dll
+app.MessageBoxA(0, '(' + x + ', ' + y + ')', 'Vrijednost (X, Y)', 0);
